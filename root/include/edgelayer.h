9383b56ab607c650635121da61ae88e6450f9ef7
edge <edge@edgedeMacBook-Pro.local>
2022-11-19 21:25:43 +0800

    [build][feat]layer define

---
+++ root/include/edgelayer.h
@@ -0,0 +1,65 @@
+class edge_layer
+{
+public:
+    virtual ~edge_layer() {}
+    virtual void layer_call() = 0;
+};
+
+class conv2d: public edge_layer
+{
+public:
+    void layer_call()
+    {
+        std::cout << "call back from conv2d layer" << std::endl;
+    }
+    conv2d(int input_dime);
+};
+conv2d::conv2d(int input_dime){
+    std::cout<<"input_dime :"<<input_dime<<std::endl;
+}
+class bn : public edge_layer
+{
+public:
+    void layer_call()
+    {
+        std::cout << "call back from bn" << std::endl;
+    }
+};
+
+class fc : public edge_layer
+{
+public:
+    void layer_call()
+    {
+        std::cout << "call back from fc" << std::endl;
+    }
+};
+enum LAYER_TYPE
+{
+    conv_2d,
+    batchnormal,
+    fullconnect
+};
+
+class LayerFactory
+{
+public:
+    edge_layer *CreateLayer(LAYER_TYPE type,int indime = 3)
+    {
+        switch (type)
+        {
+        case conv_2d:
+            return new conv2d(indime);
+            break;
+        case batchnormal:
+            return new bn();
+            break;
+        case fullconnect:
+            return new fc();
+            break;
+        default:
+            return NULL;
+            break;
+        }
+    }
+};
