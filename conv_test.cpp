ba2583cae87167c8f8959144aa82fdaee40ba5db
张一极 <zk@likedge.top>
2022-11-24 08:17:41 +0000

    [build][OPT]mutiple matrix input for conv2d->layer_call()

--- conv_test.cpp
+++ conv_test.cpp
@@ -21,7 +21,6 @@ Date：20200925
 #include "./logistic/logistic_def.h"
 #include "./autodiff/node.h"
 #include "./grad_edge/matrix_grad.h"
-
 #include "./root/include/edgelayer.h"
 #include "./welcome/score_wel.cpp"
 #include "./file_pro/data_read.h"
@@ -32,17 +31,14 @@ int main()
 {
     // welcome();
     Matrix3d mat_call = CreateMatrix3d(3, 64, 64);
-    edge_layer *conv2d_1 = new conv2d(CreateMatrix3d(3, 64, 64), 3, 6, 1, 2, 0, 0);
-    if (conv2d_1 != NULL)
-    {
-        start_t = clock();
-        Matrix3d output_conv = conv2d_1->layer_call(mat_call);
-        end_t = clock();
-        double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-        printf("CPU duration：%f\n", total_t);
-        // cout_mat3d(output_conv);
-        getshape(output_conv);
-        conv2d_1 = NULL;
-    }
+    Matrix3d mat_call2 = CreateMatrix3d(3, 64, 64);
+    edge_layer *conv2d_1 = new conv2d(CreateMatrix3d(3, 64, 64), 3, 128, 1, 2, 0, 0);
+    start_t = clock();
+    Matrix3d output_conv = conv2d_1->layer_call(mat_call2);
+    end_t = clock();
+    double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    printf("CPU duration：%f\n", total_t);
+    // cout_mat3d(output_conv);
+    getshape(output_conv);
     return 0;
 }
